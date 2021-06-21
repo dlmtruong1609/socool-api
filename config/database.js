9d7dec1f673e5a432ec7f420991bc4f4a4cf60f0
Dang Le Minh Truong <truongdlm@fpt.com.vn>
2021-06-21 21:13:30 +0700

    remove hard code db

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.adhku.mongodb.net'),
+        host: env('DATABASE_HOST'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'socool'),
-        username: env('DATABASE_USERNAME', 'socool'),
-        password: env('DATABASE_PASSWORD', 'socool123123'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
